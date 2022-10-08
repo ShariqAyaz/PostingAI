60ac22424a27bdaf7c61fb8daa4342ba5bb099c7
Shariq <gr8shariq@gmail.com>
2022-10-08 02:38:00 +0100

    posting to Store/det unposting to store/det rules are set, first time on create, first time on update point set

--- MM/admin.py
+++ MM/admin.py
@@ -4,13 +4,30 @@ from urllib.parse import urlencode
 from django.contrib import admin
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import GrnItemsDet, PaymentMethods, Warehouse, MaterialMaster, MaterialType, GrnNote
+from .models import GrnItemsDet, PaymentMethods, Warehouse, MaterialMaster, MaterialType, GrnNote, Store, StoreDet
 
 
 class GrnNoteInline(admin.TabularInline):
     model = GrnItemsDet
 
+# https://stackoverflow.com/questions/8541956/django-admin-add-extra-row-with-totals
 class GrnDetAdmin(admin.ModelAdmin):
+    list_display = (
+        'grn_no', 'itemName', 'irate', 'iqty', 'Amount'
+    )
+
+    def Amount(self, obj):
+         return 'obj.Amount'
+
+    # def get_queryset(self, request):
+    #     queryset = super().get_queryset(request)
+    #     queryset = queryset.annotate(
+    #         #_lineitem_amt=sum('irate'*'iqty')
+    #     )
+    #     return queryset
+
+
+class GrnNoteAdmin(admin.ModelAdmin):
     inlines = [
         GrnNoteInline
     ]
@@ -27,23 +44,40 @@ class GrnDetAdmin(admin.ModelAdmin):
 
             if created:
                 if current_grn.isPosted == True:
+                    
+                    Store.objects.create(ref_doc_no=current_grn.id,docType='GRN',doc_date=current_grn.date)
+
                     print('NEW\nPosted Marked ' + str(True))
                 else:
                     print('NEW\nNot Posted Marked ' + str(False))
 
             else:
-                print(current_grn.isPosted)
-                
+
                 if current_grn.isPosted == True:
                     print('UPDATED\nPosted Marked ' + str(True))
-                else:
-                    print('UPDATED\nNot Posted Marked ' + str(False))
+                    
+                    # Posting after unposting old data: 'Store' , 'StoreDet'
+                    if Store.objects.filter(ref_doc_no=current_grn.id,docType='GRN').count() > 0:
+                        print('Found in Store? matched with instance and doctype GRN')
+                    # Posting first time: 'Store' , 'StoreDet'
+                    else:
 
+                        # Storing for the first time
+                        Store.objects.create(ref_doc_no=current_grn.id,docType='GRN',doc_date=current_grn.date)
+
+                        print('Not in Store? not matched with instance id and doctype GRN')
+
+                else:
+
+                    if Store.objects.filter(ref_doc_no=current_grn.id,docType='GRN').count() > 0:
+                        print('unposting from Store and StoreDet...')
+
+                    print('UPDATED\nNot Posted Marked ' + str(False))
 
 
 admin.site.register(PaymentMethods)
 admin.site.register(Warehouse)
 admin.site.register(MaterialMaster)
 admin.site.register(MaterialType)
-admin.site.register(GrnNote,GrnDetAdmin)
-admin.site.register(GrnItemsDet)
+admin.site.register(GrnNote,GrnNoteAdmin)
+admin.site.register(GrnItemsDet, GrnDetAdmin)
--- MM/models.py
+++ MM/models.py
@@ -99,7 +99,7 @@ class GrnNote(models.Model):
     time_stamp = models.DateTimeField(auto_now=True, verbose_name="Updated at")
 
     def __str__(self):
-        return f"{self.pk}, {self.paymentMethod}, {self.invoiceNumber}, {self.vendorName}, {self.date}" 
+        return f"{self.pk}" 
 
 class GrnItemsDet(models.Model):
     grn_no = models.ForeignKey('GrnNote', models.DO_NOTHING)
@@ -108,7 +108,7 @@ class GrnItemsDet(models.Model):
     iqty = models.DecimalField(max_digits=18, decimal_places=2)
 
     def __str__(self):
-        return f"{self.grn_no}, {self.itemName}" 
+        return f"{self.grn_no}"
 #############################
 # GRN - Good Receipt Note > #
 #############################
