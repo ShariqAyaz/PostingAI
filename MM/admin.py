2655f13d014c9df27cc622ae338b7359c3cd118b
Shariq <gr8shariq@gmail.com>
2022-10-10 23:09:25 +0100

    delete function incorporate

--- MM/admin.py
+++ MM/admin.py
@@ -16,30 +16,13 @@ class GrnDetAdmin(admin.ModelAdmin):
         'grn_no', 'itemName', 'irate', 'iqty', 'Amount'
     )
 
-    @receiver(post_save)
-    def post_handler(sender, created=False, instance=None, *args, **kwargs):
-        store_doc_no = None
-        cur_grn_no = None
-
-        if sender.__name__ == 'GrnItemsDet':
-
-            cur_grn_no = instance.grn_no
-            xx = str(cur_grn_no)
-            print(cur_grn_no)
-            print(xx)
-            store_doc_no = Store.objects.filter(ref_doc_no=xx).first()
-            StoreDet.objects.create(doc=store_doc_no, itemName=instance.itemName, increase_qty=instance.iqty, decrease_qty=0)
-            
-
     def Amount(self, obj):
          return 'obj.Amount'
-
 
 class GrnNoteAdmin(admin.ModelAdmin):
     inlines = [
         GrnNoteInline
     ]
-
     list_display = ('invoiceNumber', 'vendorName', 'date', 'isPosted')
 
     @receiver(post_save)
@@ -52,7 +35,7 @@ class GrnNoteAdmin(admin.ModelAdmin):
 
             if sender.__name__ == 'GrnNote':
                 current_grn_obj = (sender.objects.get(id=instance.id))
-                cur_grn_no = current_grn_obj.id
+                cur_grn_no = instance.id
                 if created:
                     if current_grn_obj.isPosted == True:
                         store = Store.objects.create(ref_doc_no=cur_grn_no, docType='GRN', doc_date=current_grn_obj.date)
@@ -60,12 +43,16 @@ class GrnNoteAdmin(admin.ModelAdmin):
                     else:
                         print('GrnNote: On Update Event')
 
+            if sender.__name__ == 'GrnItemsDet':
+                if created:
+                    print('created invoke')
+                    print(instance.grn_no)
+                    store_doc_id = Store.objects.filter(ref_doc_no=str(instance.grn_no), docType='GRN').first()                    
+                    StoreDet.objects.create(doc=store_doc_id, itemName=instance.itemName, increase_qty=instance.iqty, decrease_qty=0)
+                else:
+                    print('GrnItemsDet: On Update Event')
+                    print('update invoke')
 
-class StoreDetAdmin(admin.ModelAdmin):
-    list_display = (
-        'doc','itemName','increase_qty','decrease_qty'
-        )
-        
 
 admin.site.register(PaymentMethods)
 admin.site.register(Warehouse)
@@ -74,4 +61,4 @@ admin.site.register(MaterialType)
 admin.site.register(GrnNote,GrnNoteAdmin)
 admin.site.register(GrnItemsDet, GrnDetAdmin)
 admin.site.register(Store)
-admin.site.register(StoreDet, StoreDetAdmin)
+admin.site.register(StoreDet)
