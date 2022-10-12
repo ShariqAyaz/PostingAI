972eb5a3e24643d1b54a6edec01cb101a410ed7a
Shariq <gr8shariq@gmail.com>
2022-10-12 22:44:21 +0100

    req.txt file added

--- MM/admin.py
+++ MM/admin.py
@@ -7,7 +7,35 @@ from django.db.models.signals import post_save, pre_delete, pre_save
 from django.dispatch import receiver
 from django.utils import timezone
 from datetime import datetime, timedelta
+from POS.models import Menuitems, Recipeitems, Recipes, Inventoryitems, Inventorytransactiondocuments, Inventorytransactions, Menuitemportions, Menuitemprices
 from .models import GrnItemsDet, PaymentMethods, Warehouse, InternalMaterial, MaterialMaster, MaterialType, GrnNote, Store, StoreDet, Products, Recipe, RecipeItems, ProcessedProduct, SaleProduction, SaleProcessedProduct, WastageProduct, WastageRawMaterial
+
+
+class MultiDBModelAdmin(admin.ModelAdmin):
+    # A handy constant for the name of the alternate database.
+    using = 'SambaPOS3'
+
+    def save_model(self, request, obj, form, change):
+        # Tell Django to save objects to the 'other' database.
+        obj.save(using=self.using)
+
+    def delete_model(self, request, obj):
+        # Tell Django to delete objects from the 'other' database
+        obj.delete(using=self.using)
+
+    def get_queryset(self, request):
+        # Tell Django to look for objects on the 'other' database.
+        return super().get_queryset(request).using(self.using)
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        # Tell Django to populate ForeignKey widgets using a query
+        # on the 'other' database.
+        return super().formfield_for_foreignkey(db_field, request, using=self.using, **kwargs)
+
+    def formfield_for_manytomany(self, db_field, request, **kwargs):
+        # Tell Django to populate ManyToMany widgets using a query
+        # on the 'other' database.
+        return super().formfield_for_manytomany(db_field, request, using=self.using, **kwargs)
 
 
 class GrnNoteInline(admin.TabularInline):
